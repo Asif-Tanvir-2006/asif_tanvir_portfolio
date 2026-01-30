8fd4aab368d8df5676c4621f3859f16740328efc
asif <asiftanvir2006@gmail.com>
2026-01-31 00:03:30 +0530

    Added links to my profiles

--- src/components/Conclusion.js
+++ src/components/Conclusion.js
@@ -5,9 +5,9 @@ function Conclusion() {
             <div className={styles.connect}>
                 <div className={styles.header}>Connect With Me!</div>
                 <div className={styles.socials}>
-                    <div className={styles.small_icon}>󰌻 </div>
-                    <div className={styles.small_icon}>󰊫 </div>
-                    <div className={styles.small_icon}>󰊤 </div>
+                    <div className={styles.small_icon}> <a href="https://www.linkedin.com/in/asif-tanvir-a68084302/">󰌻</a> </div>
+                    <div className={styles.small_icon}><a href="mailto:asiftanvir2006@gmail.com">󰊫 </a></div>
+                    <div className={styles.small_icon}><a href="https://github.com/Asif-Tanvir-2006/">󰊤</a> </div>
                 </div>
                 <div className={styles.made}>
                     ❤️ Made with love by Asif
